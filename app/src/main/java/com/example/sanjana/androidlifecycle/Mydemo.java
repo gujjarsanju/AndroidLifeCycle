e6265af7d27128a69ebc64eff7e843c51c9f7b6e
gujjarsanju <sanjana.p.gujjar@gmail.com>
2016-12-30 13:19:32 +0530

    Added Mydemoclass

---
+++ app/src/main/java/com/example/sanjana/androidlifecycle/Mydemo.java
@@ -0,0 +1,14 @@
+package com.example.sanjana.androidlifecycle;
+
+/**
+ * Created by sanjana on 30/12/16.
+ */
+
+public class Mydemo {
+
+    public Mydemo() {
+
+        System.out.println("Message");
+
+    }
+}
